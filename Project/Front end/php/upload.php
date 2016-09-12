344fe2455b94f57775563ab555675eb5774f5b40
device999 <sattari4@hotmail.com>
2016-09-12 14:23:38 +0200

    bootstrapp

--- Project/Front end/php/upload.php
+++
@@ -1,7 +0,0 @@
-<form enctype="multipart/form-data" action="fileUpload" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
-    Send the file: <input name="userfile" type="file" />
-    <input type="submit" value="Send File" />
-</form>
