71fd01004c09f0755bdbb4d40b8a83069fb4dd2d
V1nni <matyash.phil@gmail.com>
2016-10-09 01:23:09 +0200

    added sparql query controller method

--- Project/i4matcher/src/main/java/edu/unibonn/i4matcher/controllers/FileController.java
+++ Project/i4matcher/src/main/java/edu/unibonn/i4matcher/controllers/FileController.java
@@ -4,18 +4,17 @@ import java.io.ByteArrayInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 
 import javax.servlet.http.HttpServletResponse;
 
+import edu.unibonn.i4matcher.SparqlQuery;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -95,19 +94,24 @@ public class FileController {
  
 	}
 	/***************************************************
-	 * URL: /rest/controller/get/{value}
+	 * URL: /rest/controller/get?query={qry}
 	 * get(): get file as an attachment
 	 * @param response : passed by the server
-	 * @param value : value from the URL
+	 * @param qry : value from the URL
 	 * @return void
 	 ****************************************************/
-	@RequestMapping(value = "/get/{value}", method = RequestMethod.GET)
-	 public void get(HttpServletResponse response,@PathVariable String value){
-		 FileMeta getFile = files.get(Integer.parseInt(value));
-		 try {		
-			 	response.setContentType(getFile.getFileType());
-			 	response.setHeader("Content-disposition", "attachment; filename=\""+getFile.getFileName()+"\"");
-		        FileCopyUtils.copy(getFile.getBytes(), response.getOutputStream());
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	 public void get(HttpServletResponse response,
+					 //@PathVariable String value
+					 @RequestParam(value = "query") String qry){
+//		 FileMeta getFile = files.get(Integer.parseInt(value));
+		SparqlQuery sq = new SparqlQuery();
+		try {
+			String query = URLDecoder.decode(qry, "UTF-8");
+			String res = sq.getResult(query);
+			response.setContentType("application/json");
+			 	//response.setHeader("Content-disposition", "attachment; filename=\""+getFile.getFileName()+"\"");
+		        FileCopyUtils.copy(res.getBytes(), response.getOutputStream());
 		 }catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
